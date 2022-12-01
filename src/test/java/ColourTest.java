cd325891fffff72553684f40fc1b747f8b1d7b8d
james <12036321@umail.ucc.ie>
2022-12-01 20:35:57 +0000

    Started a Test to implement a single parameter constructor that takes a string of length 24 as input and chunks it up into 3 bytes of rgb values. This test is failing as the input isn't of length 4.

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ColourTest {
@@ -24,6 +25,14 @@ public class ColourTest {
         Colour testColour = new Colour(1.0f,1.0f,1.0f);
         boolean blueInRange = 0.0f <= testColour.getBlue() && testColour.getBlue() <= 1.0f;
         assertTrue(blueInRange);
+
+    }
+
+    @Test
+    public void colourSingleParamConstructorLengthOfStringTest(){
+        String input = "1111111100000000000000";
+        Colour testColour = new Colour(input);
+        assertEquals(24,input.length());
 
     }
 
