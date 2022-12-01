ee0b994c19df7620340ac99d9374f55192e28cfe
james <12036321@umail.ucc.ie>
2022-12-01 15:51:55 +0000

    Test run again for RGB range and code passes when floating points are between 0.0f and 1.0f. Thus, I must go back to the Constructor and ensure that any floating points entered are in those ranges.

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -6,22 +6,23 @@ public class ColourTest {
 
     @Test
     public void colourThreeParamConstructorRedTest(){
-        Colour testColour = new Colour(3.0f,14.0f,1.0f);
+        Colour testColour = new Colour(1.0f,1.0f,1.0f);
         boolean redInRange = 0.0f <= testColour.getRed() && testColour.getRed() <= 1.0f;
         assertTrue(redInRange);
 
     }
     @Test
     public void colourThreeParamConstructorGreenTest(){
-        Colour testColour = new Colour(3.0f,14.0f,1.0f);
+        Colour testColour = new Colour(1.0f,1.0f,1.0f);
         boolean greenInRange = 0.0f <= testColour.getGreen() && testColour.getGreen() <= 1.0f;
         assertTrue(greenInRange);
+
 
     }
 
     @Test
     public void colourThreeParamConstructorBlueTest(){
-        Colour testColour = new Colour(3.0f,14.0f,1.6f);
+        Colour testColour = new Colour(1.0f,1.0f,1.0f);
         boolean blueInRange = 0.0f <= testColour.getBlue() && testColour.getBlue() <= 1.0f;
         assertTrue(blueInRange);
 
